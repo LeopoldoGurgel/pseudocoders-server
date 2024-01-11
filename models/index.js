5c8a97e28cacc050b2107ba525daf8f4e7301122
LeopoldoGurgel <leopoldogbp@gmail.com>
2024-01-11 11:57:47 -0500

    added schema models and bcrypt library

---
+++ models/index.js
@@ -0,0 +1,6 @@
+const User = require("./User");
+const Post = require("./Post.js");
+const Comment = require("./Comment");
+const VerificationToken = require('./VerificationToken');
+
+module.exports = {User, Post, Comment, VerificationToken};
