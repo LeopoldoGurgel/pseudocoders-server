1af2fb912ff244aa9ae5a770f65bd84d389d8114
LeopoldoGurgel <leopoldogbp@gmail.com>
2024-01-11 15:12:02 -0500

    updated apollo-server to latest version

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
-const express = require('express');
-const {ApolloServer} = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
-const cors = require('cors');
-const { authMiddleware } = require('./utils/auth');
+import express from 'express';
+import {ApolloServer} from '@apollo/server';
+import {expressMiddleware} from '@apollo/server/express4';
+import {typeDefs, resolvers} from './schemas'
+import db from './config/connection';
+import cors from 'cors';
+import { authMiddleware } from './utils/auth';
+import http from 'http';
 
 const app = express();
+httpServer = http.createServer(app);
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: authMiddleware
+    plugins: [ApolloServerPluginDrainHttpServer({httpServer})]
 });
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
     await server.start();
 
-    app.use(cors());
+    app.use(
+        '/graphql',
+        cors(),
+        express.json(),
+        expressMiddleware(server, {
+            context: authMiddleware
+        }),
+        express.urlencoded({extended: true})
+        );
 
     app.get('/', (req, res) => {
         res.send('O pai ta on, Jorgim!');
@@ -26,14 +37,12 @@ const startServer = async () => {
         res.send('Welcome to my GraphQL server!');
     });
 
-    server.applyMiddleware({app, path: '/graphql'});
 
     db.on('error', console.error.bind(console, "MongoDB connection error: "))
 
-    db.once('open', () => {
-        app.listen(PORT, ()=>{
-            console.log(`Server listening on port ${PORT}`);
-        });
+    db.once('open', async () => {
+        await new Promise((resolve) => httpServer.listen({port: PORT}, resolve));
+        console.log(`Server ready at http://localhost:${PORT}/graphql`)
     });    
 };
 
