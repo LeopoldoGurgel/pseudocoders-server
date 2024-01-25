a595631c0cfe5016e127a98684da72ea73b1a502
LeopoldoGurgel <leopoldogbp@gmail.com>
2024-01-25 13:34:58 -0500

    added comments array to posts query

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -21,7 +21,7 @@ const resolvers = {
     },
     posts: async (parent, { author }) => {
       const params = author ? { author } : {};
-      return Post.find(params).sort({ createdAt: -1 }).populate('author')
+      return Post.find(params).sort({ createdAt: -1 }).populate('author').populate('comments')
     },
     post: async (parent, { postId }) => {
       return Post.findOne({ _id: postId }).populate('comments').populate('author');
